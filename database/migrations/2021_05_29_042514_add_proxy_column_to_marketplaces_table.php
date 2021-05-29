d281ee8268ae64ca56dd658276cb0b26f9567771
Astratyan Dmitry <astratyandmitry@gmail.com>
2021-05-29 10:25:45 +0600

    FIX

--- database/migrations/2021_05_29_042514_add_proxy_column_to_marketplaces_table.php
+++ database/migrations/2021_05_29_042514_add_proxy_column_to_marketplaces_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMarketplacesTable extends Migration
+class AddProxyColumnToMarketplacesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateMarketplacesTable extends Migration
      */
     public function up(): void
     {
-        Schema::create('marketplaces', function (Blueprint $table): void {
-            $table->id();
-            $table->string('key', 40)->unique()->index();
-            $table->string('title', 120)->unique();
-            $table->timestamps();
+        Schema::table('marketplaces', function (Blueprint $table): void {
+            $table->boolean('proxy')->default(false)->after('title');
         });
     }
 
@@ -28,6 +25,8 @@ class CreateMarketplacesTable extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('marketplaces');
+        Schema::table('marketplaces', function (Blueprint $table) : void{
+            $table->dropColumn('proxy');
+        });
     }
 }
